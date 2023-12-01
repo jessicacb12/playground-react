d650b0084dcee814d671aad81f47561eac89e0ce
jessicacb12 <jessica.cecilia@tokopedia.com>
2023-12-01 23:18:33 +0700

    feat: frontend mentor template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,6 @@
-import { Profiler } from "react";
-import Parent from "./Parent";
-
 export default function App() {
-  const handleRender = (
-    _id,
-    phase,
-    actualDuration,
-    baseDuration
-  ) => {
-    console.log(
-      'RERENDERING',
-      phase,
-      'ACTUAL',
-      actualDuration,
-      'BASE',
-      baseDuration
-    );
-  }
 
   return (
-    <Profiler id="Parent" onRender={handleRender}>
-      <Parent/>
-    </Profiler>
+    <div></div>
   );
 }
--- src/Title.js
+++
@@ -1,5 +0,0 @@
-function Title() {
-    console.log('Rendering title');
-    return <h1>TITLE HERE</h1>
-}
-export default Title;
